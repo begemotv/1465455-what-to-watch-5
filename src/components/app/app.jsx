c66fa062bbec234b19f64afea0fe6724daae4e29
begemotv <62621854+begemotv@users.noreply.github.com>
2020-09-30 23:21:21 +0300

    Добавлены первые компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import WelcomeScreen from "../welcome-screen/welcome-screen";
 import PropTypes from "prop-types";
+import WelcomeScreen from "../welcome-screen/welcome-screen";
 
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
 
   return (
