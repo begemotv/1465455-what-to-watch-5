f400a34ac194d1b59c432fb66d1120c3bfda2b89
begemotv <62621854+begemotv@users.noreply.github.com>
2020-10-04 16:38:31 +0300

    Добавили новые экраны и настроили маршруты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import GameOverScreen from "../game-over-screen/game-over-screen";
+import LoginScreen from "../login-screen/login-screen";
+import QuestionArtistScreen from "../question-artist-screen/question-artist-screen";
+import QuestionGenreScreen from "../question-genre-screen/question-genre-screen";
+import WinScreen from "../win-screen/win-screen";
 
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount} />
+        </Route>
+        <Route exact path="/login">
+          <LoginScreen />
+        </Route>
+        <Route exact path="/dev-artist">
+          <QuestionArtistScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <QuestionGenreScreen />
+        </Route>
+        <Route exact path="/win">
+          <WinScreen />
+        </Route>
+        <Route exact path="/lose">
+          <GameOverScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 
